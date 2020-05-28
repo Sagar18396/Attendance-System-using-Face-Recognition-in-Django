53e9bb9b158152ba08bff62bd9072cadf51f3ea5
Sagar18396 <sam.18396@gmail.com>
2020-05-28 19:30:00 +0530

    added whitenoise to settings file

--- AttendanceSystem/settings.py
+++ AttendanceSystem/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleWare',
 ]
 
 ROOT_URLCONF = 'AttendanceSystem.urls'
