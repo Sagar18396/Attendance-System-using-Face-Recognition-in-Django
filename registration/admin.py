96c04afbb41cb5d26c8fc90638140aee7f831504
Sagar18396 <sam.18396@gmail.com>
2020-05-28 21:03:30 +0530

    added classes to admin

--- registration/admin.py
+++ registration/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ProductKey
+from .models import ProductKey, AdminDetail, Employee
 # Register your models here.
 
 
 admin.site.register(ProductKey)
+admin.site.register(AdminDetail)
+admin.site.register(Employee)
