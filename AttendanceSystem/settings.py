df9ed29d052642e1a125d541e3a9c201cc5b8b02
Sagar18396 <sam.18396@gmail.com>
2020-05-28 19:54:41 +0530

    added django-heroku to settings and requirements

--- AttendanceSystem/settings.py
+++ AttendanceSystem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,4 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-    
+
+django_heroku.settings(locals())
